6a28641193f87433789e8edfe585d4afa3c8235d
datmt <datmt2003@vnpt.vn>
2026-01-03 09:26:53 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,7 @@ const startServer = async () => {
     await authenticateDatabase();
     await syncDatabase();
     // await redisConnection();
-    app.listen(port, () => {
+    app.listen(port, "0.0.0.0", () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (err) {
